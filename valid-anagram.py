e723e1aba170f946333659aa87016f1b6f7aaa6d
Dmitriy <109631442+DimaCoderNow@users.noreply.github.com>
2025-05-04 14:16:34 +0200

    add fast solution

--- valid-anagram.py
+++ valid-anagram.py
@@ -8,3 +8,12 @@ def isAnagram(self, s: str, t: str) -> bool:
         s_dict[s_char] = s_dict.get(s_char, 0) + 1
         t_dict[t_char] = t_dict.get(t_char, 0) + 1
     return s_dict == t_dict
+
+
+def isAnagram(self, s, t):
+    if len(s) != len(t):
+        return False
+    for char in set(s):
+        if s.count(char) != t.count(char):
+            return False
+    return True
